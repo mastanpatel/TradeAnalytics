468372f2cead7c3e7c7d97d11f7666893fdb1dae
Mastan Patel <m.mastanpatel@gmail.com>
2022-07-03 09:40:33 +0530

    added entities

---
+++ TradeAnalytics.Domain/Entities/TradeFee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TradeAnalytics.Domain.Common;
+
+namespace TradeAnalytics.Domain.Entities
+{
+    internal class TradeFee : AuditableEntity
+    {
+    }
+}
