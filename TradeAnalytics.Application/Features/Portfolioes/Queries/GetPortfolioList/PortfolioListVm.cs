71db36e96cc6c508f2aacb7f95b53ed6096bd933
Mastan Patel <m.mastanpatel@gmail.com>
2022-08-18 16:34:01 +0530

    changes made to requist get all portfolio ferom guid to int

--- TradeAnalytics.Application/Features/Portfolioes/Queries/GetPortfolioList/PortfolioListVm.cs
+++ TradeAnalytics.Application/Features/Portfolioes/Queries/GetPortfolioList/PortfolioListVm.cs
@@ -6,7 +6,7 @@ namespace TradeAnalytics.Application.Features.Portfolioes.Queries.GetPortfolioLi
 {
     public class PortfolioListVm
     {
-        public Guid PortfolioId { get; set; }
+        public int PortfolioId { get; set; }
         public string Name { get; set; }
         public string Desc { get; set; }
         public bool IsActive { get; set; }
