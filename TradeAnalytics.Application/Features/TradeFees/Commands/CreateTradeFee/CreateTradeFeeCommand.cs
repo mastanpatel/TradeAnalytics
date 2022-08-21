4899f53bd415ee4e2d1f88eba115a99fb8f7086d
Mastan Patel <m.mastanpatel@gmail.com>
2022-08-21 07:37:33 +0530

    resolved build error

--- TradeAnalytics.Application/Features/TradeFees/Commands/CreateTradeFee/CreateTradeFeeCommand.cs
+++ TradeAnalytics.Application/Features/TradeFees/Commands/CreateTradeFee/CreateTradeFeeCommand.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using TradeAnalytics.Application.Features.TradeFees.Queries.GetTradeFeeDetail;
 using TradeAnalytics.Domain.Entities.TradeFee;
 
 namespace TradeAnalytics.Application.Features.TradeFees.Commands.CreateTradeFee
