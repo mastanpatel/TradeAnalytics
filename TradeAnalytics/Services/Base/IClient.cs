c22acea9ac4d8403cc81b5bc809c022c7c97c50c
mastan patel <mastanpatel@19932021.onmicrosoft.com>
2023-03-11 12:38:53 +0530

    added angular UI

--- TradeAnalytics/Services/Base/IClient.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Net.Http;
-
-namespace TradeAnalytics.Services.Base
-{
-    public partial interface IClient
-    {
-        public HttpClient HttpClient { get; }
-    }
-}
--- TradeAnalytics/ViewModels/LoginViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TradeAnalytics.ViewModels
-{
-    public class LoginViewModel
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
